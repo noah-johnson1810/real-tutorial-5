26bf7dc968566147203802ca9b4ecf0b3084d548
Noah Johnson <njohnson@omnitech-inc.com>
2024-03-22 15:12:38 -0600

    Initial part of tutorial 6 done

--- app/src/main/java/edu/sdsmt/johnson_n_a/tutorial56/Cloud.java
+++ app/src/main/java/edu/sdsmt/johnson_n_a/tutorial56/Cloud.java
@@ -18,12 +18,48 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Random;
 
 public class Cloud {
+
+    private final static FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private final static DatabaseReference hattingsList = database.getReference("hattings");
+
     /**
-     * A class that holds a line's contents for later updating
+     * method to load a specific hatting
+     *
+     * @param view  The view we are loading the hatting into
+     * @param catId the id of the hatting
+     * @param dlg   the dialog box showing the loading state
+     */
+    public void loadFromCloud(final HatterView view, String catId, final Dialog dlg) {
+
+
+        DatabaseReference myRef = hattingsList.child(catId);
+
+        // Read from the database
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                view.loadJSON(dataSnapshot);
+                dlg.dismiss();
+                if (view.getContext() instanceof HatterActivity)
+                    ((HatterActivity) view.getContext()).updateUI();
+                view.invalidate();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Error condition!
+                view.post(() -> {
+                    Toast.makeText(view.getContext(), R.string.loading_fail, Toast.LENGTH_SHORT).show();
+                    dlg.dismiss();
+                });
+            }
+        });
+    }
+
+    /**
+     * An class that holds a line's contents for later updating
      */
     public static class ViewHolder extends RecyclerView.ViewHolder {
         // each data line
@@ -48,12 +84,12 @@ public class Cloud {
      * the cloud server.
      */
     public static class CatalogAdapter extends RecyclerView.Adapter<ViewHolder> {
-
         /**
          * The items we display in the list box. Initially this is
          * null until we get items from the server.
          */
-        private ArrayList<Item> items = new ArrayList<>();
+        private final ArrayList<Item> items;
+
         public final CatalogCallback clickEvent;
 
         /**
@@ -64,47 +100,14 @@ public class Cloud {
             clickEvent = click;
         }
 
-        public Item getItem(int position) {
-            return items.get(position);
-        }
-
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @NonNull
-        @Override
-        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.catalog_item, parent, false);
-            return new ViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-            holder.view.setOnClickListener((v -> {
-                Item catItem = getItem(holder.getAdapterPosition());
-
-                // let the client class do its job
-                clickEvent.callback(catItem);
-            }));
-
-            TextView tv = holder.view.findViewById(R.id.textItem);
-            String text = items.get(position).name;
-            tv.setText(text);
-        }
-
-        @Override
-        public int getItemCount() {
-            return items.size();
-        }
-
         public ArrayList<Item> getCatalog(final View view) {
             ArrayList<Item> newItems = new ArrayList<>();
 
             //connect to the database (hattings child)
-            FirebaseDatabase database = FirebaseDatabase.getInstance();
+
             database.goOnline();
-            DatabaseReference myRef = database.getReference("hattings");
+            DatabaseReference myRef = hattingsList;
+
 
             // Read from the database
             myRef.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -131,49 +134,51 @@ public class Cloud {
 
                 @Override
                 public void onCancelled(@NonNull DatabaseError error) {
-                    // Failed to read value
+                    // Error condition!
                     view.post(() -> Toast.makeText(view.getContext(), R.string.catalog_fail, Toast.LENGTH_SHORT).show());
                 }
             });
-
             return newItems;
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.catalog_item, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+            holder.view.setOnClickListener(v -> {
+                Item catItem = getItem(position);
+
+                // let the client class do its job
+                clickEvent.callback(catItem);
+            });
+
+            TextView tv = holder.view.findViewById(R.id.textItem);
+            String text = items.get(position).name;
+            tv.setText(text);
+        }
+
+        @Override
+        public int getItemCount() {
+            return items.size();
+        }
+
+        public Item getItem(int position) {
+            return items.get(position);
+        }
+
+        public long getItemId(int position) {
+            return position;
         }
 
         public interface CatalogCallback {
             void callback(Cloud.Item catItem);
         }
-    }
-
-    /**
-     * method to load a specific hatting
-     *
-     * @param view  The view we are loading the hatting into
-     * @param catId the id of the hatting
-     * @param dlg   the dialog box showing the loading state
-     */
-    public void loadFromCloud(final HatterView view, String catId, final Dialog dlg) {
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("hattings").child(catId);
-
-        // Read from the database
-        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                view.loadJSON(dataSnapshot);
-                dlg.dismiss();
-                if (view.getContext() instanceof HatterActivity)
-                    ((HatterActivity) view.getContext()).updateUI();
-                view.invalidate();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                view.post(() -> {
-                    Toast.makeText(view.getContext(), R.string.loading_fail, Toast.LENGTH_SHORT).show();
-                    dlg.dismiss();
-                });
-            }
-        });
     }
 
     /**
@@ -197,13 +202,13 @@ public class Cloud {
             hattingsList = hattingsList.child(key);
             DatabaseReference finalHattingsList = hattingsList;
             hattingsList.child("name").setValue(name, (databaseError, databaseReference) -> {
-                if(databaseError != null) {
+                if (databaseError != null) {
                     // Error condition!
                     /*
                      * make a toast
                      */
                     Toast.makeText(view.getContext(), "Unable to Save Data", Toast.LENGTH_SHORT).show();
-                }else{
+                } else {
                     view.saveJSON(finalHattingsList);
                 }
             });
